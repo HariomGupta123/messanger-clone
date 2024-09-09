da76a26b9e3e2e7621a8bd36327e30e683195f92
HariomGupta123 <hg7716505@gmail.com>
2024-09-09 12:50:00 +0545

    debubbing

--- src/app/api/auth/[nextauth]/route.ts
+++ src/app/api/auth/[nextauth]/route.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
  // Import from the new file
 
 const handler = NextAuth(authOptions);
+export { authOptions };
 
 // Export the HTTP methods handled by this route
 export { handler as GET, handler as POST };
